58695172cba636f0f4a9ebaa16c70e5e3fabb572
Ndi-Shema <f.shema@alustudent.com>
2023-03-10 11:09:41 +0000

    web server preps

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Importing from different sources"""
+
+import os
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Packs web_static files in a tgz archive."""
+    if not os.path.isdir("versions"):
+        os.makedirs("versions")
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(timestamp)
+
+    command = "tar -cvzf {} web_static".format(archive_path)
+
+    if result.failed:
+        return None
+    else:
+        return archive_path
