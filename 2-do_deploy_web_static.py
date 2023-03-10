66fe2a793aee37179a9b6177badb890a6fddd7c9
Ndi-Shema <f.shema@alustudent.com>
2023-03-10 12:58:56 +0000

    deploying

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,78 @@
-#!/usr/bin/python3
-"""Importing from better sources"""
-from fabric.api import *
-import os
-import re
-from datetime import datetime
+#!/usr/bin/env python3
 
-env.user = 'ubuntu'
-env.hosts = ['75.101.238.212', '54.85.162.84']
+import datetime
+import os
+import shutil
+import subprocess
+
+from fabric import Connection
+
+
+# Server configuration
+SERVERS = ['75.101.238.212', '54.85.162.84']
+REMOTE_USER = 'ubuntu'
+REMOTE_DIR = '/data/web_static'
+CURRENT_LINK = os.path.join(REMOTE_DIR, 'current')
+BACKUP_DIR = os.path.join(REMOTE_DIR, 'backups')
 
 
 def do_pack():
-    """timestamp"""
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = fversions/web_static_{timestamp}.tgz
-    local("mkdir -p versions")
-    with hide('running'):
-        result = local(ftar -czvf {archive_name} web_static/)
-    if result.failed:
+    """Create a compressed archive of the web_static directory."""
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = f'web_static_{timestamp}.tgz'
+    local_dir = os.path.abspath('web_static')
+    local_archive = os.path.join(os.path.abspath('versions'), archive_name)
+
+    os.makedirs(os.path.dirname(local_archive), exist_ok=True)
+
+    with subprocess.Popen(
+            ['tar', '-czf', local_archive, '-C', local_dir, '.'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+    ) as proc:
+        out, err = proc.communicate()
+
+    if proc.returncode != 0:
+        print(f'Error creating archive: {err.decode()}')
         return None
-    return archive_name
+
+    print(f'Created archive: {local_archive}')
+    return local_archive
 
 
 def do_deploy(archive_path):
-    """deploying the saved archives"""
+    """Upload and extract the archive on the remote server."""
     if not os.path.isfile(archive_path):
+        print(f'Archive not found: {archive_path}')
         return False
-    #trying to upload all the archive names to the server
-    archive_filename = os.path.splitext(os.path.basename(archive_path))[0]
-    archive_remote_path = f/tmp/{archive_filename}.tgz
-    try:
-        put(archive_path, archive_remote_path)
-        run(fmkdir -p /data/web_static/releases/{archive_filename})
-        run(ftar -xzf {archive_remote_path} 
-            f-C /data/web_static/releases/{archive_filename}/ 
-            --strip-components 1)
-        run(frm {archive_remote_path})
-        run(fmv /data/web_static/releases/{archive_filename}/web_static/* 
-            f/data/web_static/releases/{archive_filename}/)
-        run(frm -rf /data/web_static/releases/{archive_filename}/web_static)
-        run(frm -rf /data/web_static/current)
-        run(fln -s /data/web_static/releases/{archive_filename} 
-            f/data/web_static/current)
-        return True
-    except Exception:
-        return False
+
+    archive_name = os.path.basename(archive_path)
+    remote_archive = os.path.join('/tmp', archive_name)
+    c = Connection(REMOTE_USER, SERVERS[0])
+
+    # Create backups directory if it doesn't exist
+    c.run(f'mkdir -p {BACKUP_DIR}')
+
+    # Upload archive to remote server
+    c.put(archive_path, remote_archive)
+
+    # Extract archive to temporary directory
+    temp_dir = os.path.join(REMOTE_DIR, 'tmp')
+    c.run(f'mkdir -p {temp_dir}')
+    c.run(f'tar -xzf {remote_archive} -C {temp_dir}')
+
+    # Create new release directory and move files
+    release_dir = os.path.join(REMOTE_DIR, 'releases', archive_name[:-4])
+    c.run(f'mkdir -p {release_dir}')
+    c.run(f'mv {temp_dir}/web_static/* {release_dir}')
+
+    # Remove temporary directory and archive
+    c.run(f'rm -rf {temp_dir}')
+    c.run(f'rm {remote_archive}')
+
+    # Update symbolic link
+    c.run(f'rm -f {CURRENT_LINK}')
+    c.run(f'ln -s {release_dir} {CURRENT_LINK}')
+
+    print('Deployment successful!')
+    return True
