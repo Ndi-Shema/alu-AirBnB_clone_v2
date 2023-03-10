48a5a5262b7e63e10e3da0d21c94c561785b7cf9
Ndi-Shema <f.shema@alustudent.com>
2023-03-10 09:32:22 +0000

    Nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '37i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
-sudo service nginx restart
+# Check if nginx is installed, and install it if not
+if ! dpkg -s nginx &> /dev/null; then
+  sudo apt-get update
+  sudo apt-get -y install nginx
+fi
+
+# Create directories for web static content
+sudo mkdir -p /var/www/html/hbnb_static/releases/test/
+sudo mkdir -p /var/www/html/hbnb_static/shared/
+
+# Create a symbolic link for web static content
+sudo ln -sf /var/www/html/hbnb_static/releases/test/ /var/www/html/hbnb_static/current
+
+# Create an index.html file with test content
+sudo sh -c 'echo "<html><head><title>Test page</title></head><body><p>This is a test page.</p></body></html>" > /var/www/html/hbnb_static/releases/test/index.html'
+
+# Set ownership for directories
+sudo chown -R www-data:www-data /var/www/html/hbnb_static
+
+# Add location block for web static content to the nginx config
+sudo sh -c 'echo "location /hbnb_static/ {\n\talias /var/www/html/hbnb_static/current/;\n}\n" > /etc/nginx/sites-available/hbnb_static'
+sudo ln -sf /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/hbnb_static
+sudo nginx -t && sudo systemctl reload nginx
