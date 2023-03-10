a6601b5eb992ea0a3c0c05e9afe6ac08b8267604
Ndi-Shema <f.shema@alustudent.com>
2023-03-10 09:58:17 +0000

    Nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,7 +13,7 @@ echo "<html>
   <head>
   </head>
   <body>
-    Holberton School
+    Test this Nginx
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
@@ -22,7 +22,6 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 # Set ownership and permissions
 sudo chown -R ubuntu:ubuntu /data
-sudo chmod -R 755 /data
 
 # Configure Nginx to serve the static file
 sudo sed -i '/server_name _;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n    }' /etc/nginx/sites-available/default
