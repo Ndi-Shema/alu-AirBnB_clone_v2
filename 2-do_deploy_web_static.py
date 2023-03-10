75a1125cd486d85afa239479e85d5d5328180865
Ndi-Shema <f.shema@alustudent.com>
2023-03-10 16:34:16 +0000

    deploying

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,65 @@
 #!/usr/bin/python3
-import os.path
+
+import os
+import datetime
 from fabric import Connection
+from invoke import Responder
 
-env.hosts = ["75.101.238.212", "54.85.162.84"]
+env = {
+    'hosts': ['75.101.238.212', '54.85.162.84'],
+    'user': 'ubuntu',
+    'key_filename': '~/.ssh/id_rsa'
+}
 
+def do_pack():
+    """
+    Compress the web_static folder and save the archive
+    in the versions folder with a timestamp.
+    """
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f"web_static_{now}.tgz"
+    os.makedirs("versions", exist_ok=True)
+    os.system(f"tar -czvf versions/{filename} web_static/")
+    return f"versions/{filename}"
 
-def do_deploy(archive_path): 
-  """deploying archives to the server."""
-  
-    if os.path.isfile(archive_path) is False:
+def do_deploy(archive_path):
+    """
+    Distribute the archive to the web servers.
+    """
+    if not os.path.isfile(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    with Connection(env.hosts[0]) as conn:
-        put_result = conn.put(archive_path, "/tmp/{}".format(file))
-        if put_result.failed is True:
-            return False
+    # Upload archive to remote server
+    conn = Connection(env.hosts[0], user=env.user, connect_kwargs={"key_filename": env.key_filename})
+    put_result = conn.put(archive_path, "/tmp/")
+    if put_result.failed:
+        return False
 
-        if conn.run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-            return False
-        if conn.run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-            return False
-        if conn.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-            return False
-        if conn.run("rm /tmp/{}".format(file)).failed is True:
-            return False
-        if conn.run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-            return False
-        if conn.run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-            return False
-        if conn.run("rm -rf /data/web_static/current").failed is True:
-            return False
-        if conn.run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-            return False
-        return True
+    # Create remote directory to store the files
+    name = archive_path.split("/")[-1].split(".")[0]
+    remote_path = f"/data/web_static/releases/{name}"
+    if conn.run(f"mkdir -p {remote_path}").failed:
+        return False
+
+    # Extract files from the archive
+    if conn.run(f"tar -xzf /tmp/{name}.tgz -C {remote_path}").failed:
+        return False
+
+    # Delete archive from the remote server
+    if conn.run(f"rm /tmp/{name}.tgz").failed:
+        return False
+
+    # Move files to the final location
+    if conn.run(f"mv {remote_path}/web_static/* {remote_path}").failed:
+        return False
+
+    # Remove old symlink
+    if conn.run(f"rm -rf /data/web_static/current").failed:
+        return False
+
+    # Create new symlink
+    if conn.run(f"ln -s {remote_path} /data/web_static/current").failed:
+        return False
+
+    return True
+
