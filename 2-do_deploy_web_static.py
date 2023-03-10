83631c8f6eb22773f1c09525f7eebc98c002eac3
Ndi-Shema <106658796+Ndi-Shema@users.noreply.github.com>
2023-03-10 17:33:54 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,55 +1,52 @@
 #!/usr/bin/python3
-"""Comment"""
+# Compress the web_static folder and save the archive in the versions folder with a timestamp.
 import os
-import re
-from datetime import datetime
+import datetime
 from fabric import Connection
 from invoke import Responder
 
-env.user = 'ubuntu'
-env.hosts = ['3.80.74.138', '3.88.68.105']
-
+env = {'hosts': ['75.101.238.212', '54.85.162.84'], 'user': 'ubuntu', 'key_filename': '~/.ssh/id_rsa'}
 
 def do_pack():
-    """Comm"""
-    local("mkdir -p versions")
-    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                   capture=True)
-    if result.failed:
-        return None
-    return result
-
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f"web_static_{now}.tgz"
+    os.system("mkdir -p versions")
+    os.system(f"tar -czvf versions/{filename} web_static/")
+    return f"versions/{filename}"
 
 def do_deploy(archive_path):
-    """Comment"""
     if not os.path.isfile(archive_path):
         return False
 
-    filename_regex = re.compile(r'[^/]+(?=\.tgz$)')
-    match = filename_regex.search(archive_path)
+    with Connection(env.hosts[0], user=env['user'], connect_kwargs={"key_filename": env['key_filename']}) as conn:
+        put_result = conn.put(archive_path, "/tmp/")
+        if put_result.failed:
+            return False
 
-    # Upload the archive to the /tmp/ directory of the web server
-    archive_filename = match.group(0)
-    remote_path = "/tmp/{}.tgz".format(archive_filename)
-    c = Connection(host=env.hosts[0], user=env.user)
-    c.put(archive_path, remote=remote_path)
+    with Connection(env.hosts[0], user=env['user'], connect_kwargs={"key_filename": env['key_filename']}) as conn:
+        name = archive_path.split("/")[-1].split(".")[0]
+        remote_path = f"/data/web_static/releases/{name}"
+        if conn.run(f"mkdir -p {remote_path}").failed:
+            return False
 
-    # Uncompress the archive to the folder
-    #     /data/web_static/releases/<archive filename without extension> on
-    #     the web server
-    remote_dir = "/data/web_static/releases/{}/".format(archive_filename)
-    c.run("sudo mkdir -p {}".format(remote_dir))
-    c.run("sudo tar -xzf {} -C {} --strip-components=1".format(remote_path, remote_dir))
+    with Connection(env.hosts[0], user=env['user'], connect_kwargs={"key_filename": env['key_filename']}) as conn:
+        if conn.run(f"tar -xzf /tmp/{name}.tgz -C {remote_path}").failed:
+            return False
 
-    # Delete the archive from the web server
-    c.run("sudo rm {}".format(remote_path))
+    with Connection(env.hosts[0], user=env['user'], connect_kwargs={"key_filename": env['key_filename']}) as conn:
+        if conn.run(f"rm /tmp/{name}.tgz").failed:
+            return False
 
-    # Move the contents of web_static to the new folder
-    c.run("sudo mv {} /data/web_static/releases/{}/".format(remote_dir + "web_static/*", archive_filename))
+    with Connection(env.hosts[0], user=env['user'], connect_kwargs={"key_filename": env['key_filename']}) as conn:
+        if conn.run(f"mv {remote_path}/web_static/* {remote_path}").failed:
+            return False
 
-    # Remove the old symbolic link and create a new one
-    c.run("sudo rm -rf /data/web_static/current")
-    c.run("sudo ln -s {} /data/web_static/current".format(remote_dir))
+    with Connection(env.hosts[0], user=env['user'], connect_kwargs={"key_filename": env['key_filename']}) as conn:
+        if conn.run(f"rm -rf /data/web_static/current").failed:
+            return False
+
+    with Connection(env.hosts[0], user=env['user'], connect_kwargs={"key_filename": env['key_filename']}) as conn:
+        if conn.run(f"ln -s {remote_path} /data/web_static/current").failed:
+            return False
 
     return True
